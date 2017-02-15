5113efb2e5f746134d16581960dd6f0764d5b5d7
smbatchou <stephabio@yahoo.ca>
2017-02-15 11:09:21 -0500

    refactor

--- ipaq.r
+++ ipaq.r
@@ -16,11 +16,18 @@ dt_ <- copy(full_d_)
 
 
 
-#Run ipaq short then ipaq long
+#Run ipaq short 
 source('ipaq_short.r')
-source('ipaq_long.r')
+ipaq_rm(dt_,vars = names(dt_)[grepl('_WK$', names(dt_))] )
 
-# harmonize PA_TOTAL and PA_LEVEL
+#then  run ipaq long
+source('ipaq_long.r')
+source('ipaq_long2.r')
+ipaq_rm( dt_,vars = names(dt_)[grepl('_WK$|_PARTIAL_|_PROP$|(DAY_EST(_FLOOR)?)$', names(dt_))] )
+
+
+# clean gt 960*7
+ipaq_clean_gt960_7(dt_,vars = names(dt_)[!grepl('time|freq|job|ID|A_ADM', names(dt_))] )
 
 
 #The final ipaq table is in dt_ and can be saved as a csv
