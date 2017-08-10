78c022a88d58a277b7fc8624b30cbe461f263ea1
smbatchou <stephabio@yahoo.ca>
2017-08-10 13:10:15 -0400

    keep STUDY_NAME safe

--- ipaq.r
+++ ipaq.r
@@ -27,8 +27,8 @@ ipaq_rm( dt_,vars = names(dt_)[grepl('_WK$|_PARTIAL_|_PROP$|(DAY_EST(_FLOOR)?)$'
 
 
 # clean gt 960*7
-ipaq_clean_gt960_7(dt_,vars = names(dt_)[!grepl('time|freq|job|ID|A_ADM', names(dt_))] )
+ipaq_clean_gt960_7(dt_,vars = names(dt_)[!grepl('time|freq|job|(?i)ID|A_ADM|STUDY_NAME', names(dt_))] )
 
 
 #The final ipaq table is in dt_ and can be saved as a csv
-fwrite(dt_,file = 'ipaq_final.csv',sep = ',',row.names = FALSE,quote = TRUE,verbose = T,na = '')
+fwrite(dt_,file = 'ipaq_final.csv',row.names = FALSE,quote = TRUE,verbose = T,na = '')
